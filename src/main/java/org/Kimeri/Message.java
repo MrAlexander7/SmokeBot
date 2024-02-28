c377ebe19ba3918f6ae63d3ed42195c2903398f4
MrAlexander7 <sasha-yarifa@ukr.net>
2024-02-28 23:48:14 +0200

    Add base message

---
+++ src/main/java/org/Kimeri/Message.java
@@ -0,0 +1,14 @@
+package org.Kimeri;
+
+public class Message {
+    public static final String TOKENBOT = "6993230113:AAFsj_ytf_5VOfcdwCvGx7vLrbrHQ2eF7YE";
+    public static final String BOTNAME = "@SmokeBuyBot";
+    public static final String STARTMESSAGEUSER = "";
+
+    public static final String STARTMESSAGEADMIN = "";
+
+    public static final String ADMINID = "";
+
+
+
+}
