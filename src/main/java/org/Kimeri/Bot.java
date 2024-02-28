81137cef3348c544388b8939a32a7db9d0bba1d8
MrAlexander7 <sasha-yarifa@ukr.net>
2024-02-28 23:47:57 +0200

    Start code

--- src/main/java/org/Kimeri/Bot.java
+++ src/main/java/org/Kimeri/Bot.java
@@ -1,4 +1,21 @@
 package org.Kimeri;
 
-public class Bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
 }
