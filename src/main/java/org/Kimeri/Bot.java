f73defb1d8268e16ddb5d158c2d8222521e7e1c0
MrAlexander7 <sasha-yarifa@ukr.net>
2024-02-29 00:04:19 +0200

    Mini test to deploy (failed)

--- src/main/java/org/Kimeri/Bot.java
+++ src/main/java/org/Kimeri/Bot.java
@@ -1,21 +1,30 @@
 package org.Kimeri;
 
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class Bot extends TelegramLongPollingBot {
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            execute(SendMessage
+                    .builder()
+                    .chatId(update.getMessage().getChatId().toString())
+                    .text(update.getMessage().getText())
+                    .build());
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return Message.BOTNAME;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return Message.TOKENBOT;
     }
 }
--- src/main/java/org/Kimeri/Main.java
+++ src/main/java/org/Kimeri/Main.java
@@ -1,7 +1,14 @@
 package org.Kimeri;
 
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
+    @SneakyThrows
     public static void main(String[] args) {
-        
+        Bot bot = new Bot();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(bot);
     }
 }
