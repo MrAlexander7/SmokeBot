8b2169ce4f5f8a273c410fd3fa6cf9e756a21d06
MrAlexander7 <sasha-yarifa@ukr.net>
2024-07-30 15:15:32 +0300

    1 Variant (Final)

--- src/main/java/org/Kimeri/SendMessages.java
+++ src/main/java/org/Kimeri/SendMessages.java
@@ -1,4 +1,103 @@
 package org.Kimeri;
 
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
 public class SendMessages {
+
+    Bot bot = new Bot();
+    SendMessage message = new SendMessage();
+    EditMessageText editMessageText = new EditMessageText();
+    DataBase dataBase = new DataBase();
+
+
+    @SneakyThrows
+    public void sendStartMessage(Update update) {
+        System.out.println("start");
+        message.setChatId(update.getMessage().getChatId());
+        message.setText(MessageUser.STARTMESSAGEUSER);
+        message.setReplyMarkup(Buttons.mainButtons());
+        bot.execute(message);
+    }
+
+    @SneakyThrows
+    public void sendCatalog(Update update, CallbackQuery callbackQuery) {
+        System.out.println("catalog");
+        dataBase.initCursor();
+        DataBase.shownDocuments.clear();
+        Message messages = (Message) callbackQuery.getMessage();
+        String chatId = callbackQuery.getMessage().getChatId().toString();
+        Integer messageId = messages.getMessageId();
+        editMessageText.setChatId(chatId);
+        editMessageText.setMessageId(messageId);
+        editMessageText.setText(MessageUser.CATALOG + dataBase.viewData(chatId, callbackQuery.getId(), messageId, "/catalog:"));
+        editMessageText.setReplyMarkup(Buttons.catalogButtons());
+        bot.execute(editMessageText);
+    }
+
+    @SneakyThrows
+    public void sendHelp(Update update, CallbackQuery callbackQuery) {
+        System.out.println("help");
+        Message messages = (Message) callbackQuery.getMessage();
+        editMessageText.setChatId(callbackQuery.getMessage().getChatId());
+        editMessageText.setMessageId(messages.getMessageId());
+        editMessageText.setText(MessageUser.HELP);
+        editMessageText.setReplyMarkup(Buttons.backButton());
+        bot.execute(editMessageText);
+
+    }
+
+    @SneakyThrows
+    public void sendContact(Update  update, CallbackQuery callbackQuery) {
+        System.out.println("contact");
+        Message messages = (Message) callbackQuery.getMessage();
+        editMessageText.setChatId(callbackQuery.getMessage().getChatId());
+        editMessageText.setMessageId(messages.getMessageId());
+        editMessageText.setText(MessageUser.CONTACT);
+        editMessageText.setReplyMarkup(Buttons.backButton());
+        bot.execute(editMessageText);
+
+    }
+
+
+    @SneakyThrows
+    public void sendBack(Update update , CallbackQuery callbackQuery) {
+        System.out.println("back");
+        Message messages = (Message) callbackQuery.getMessage();
+        editMessageText.setChatId(callbackQuery.getMessage().getChatId());
+        editMessageText.setMessageId(messages.getMessageId());
+        editMessageText.setText(MessageUser.STARTMESSAGEUSER);
+        editMessageText.setReplyMarkup(Buttons.mainButtons());
+        bot.execute(editMessageText);
+        DataBase.cursor.close();
+        DataBase.cursor = null;
+        DataBase.currentPage = 0;
+    }
+
+    @SneakyThrows
+    public void sendNext(Update update, CallbackQuery callbackQuery) {
+        System.out.println("nextPage");
+        Message messages = (Message) callbackQuery.getMessage();
+        editMessageText.setChatId(callbackQuery.getMessage().getChatId());
+        editMessageText.setMessageId(messages.getMessageId());
+        editMessageText.setText(MessageUser.CATALOG + dataBase.viewData(messages.getChatId().toString(), update.getCallbackQuery().getId(), messages.getMessageId(), update.getCallbackQuery().getData()));
+        editMessageText.setReplyMarkup(Buttons.catalogButtons());
+        bot.execute(editMessageText);
+    }
+
+    @SneakyThrows
+    public void sendBackPage(Update update, CallbackQuery callbackQuery) {
+        System.out.println("backPage");
+        Message messages = (Message) callbackQuery.getMessage();
+        editMessageText.setChatId(callbackQuery.getMessage().getChatId());
+        editMessageText.setMessageId(messages.getMessageId());
+        editMessageText.setText(MessageUser.CATALOG + dataBase.viewData(messages.getChatId().toString(), update.getCallbackQuery().getId(), messages.getMessageId(), update.getCallbackQuery().getData()));
+        editMessageText.setReplyMarkup(Buttons.catalogButtons());
+        bot.execute(editMessageText);
+    }
 }
--- src/main/java/org/Kimeri/UserHandle.java
+++ src/main/java/org/Kimeri/UserHandle.java
@@ -1,70 +1,25 @@
 package org.Kimeri;
 
 import lombok.SneakyThrows;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class UserHandle {
-    Bot bot = new Bot();
-    SendMessage message = new SendMessage();
-
-    DataBase dataBase = new DataBase();
-
-    int backPage = 0;
+    SendMessages sendMessages = new SendMessages();
 
     @SneakyThrows
     public void handle(Update update) {
-        switch (update.getMessage().getText()) {
-            case "/start":
-                System.out.println("start");
-                if (DataBase.editMode == 1) {
-                    DataBase.editMode = 0;
-                    DataBase.cursor.close();
-                    DataBase.cursor = null;
-                }
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText(MessageUser.STARTMESSAGEUSER);
-                message.setReplyMarkup(Buttons.mainButtons());
-                bot.execute(message);
-                break;
-            case "/help":
-                System.out.println("help");
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText(MessageUser.HELP);
-                message.setReplyMarkup(Buttons.backButton());
-                bot.execute(message);
-                break;
-            case "/catalog":
-                System.out.println("catalog");
-                String id = update.getMessage().getChatId().toString();
-                message.setChatId(id);
-                message.setText(MessageUser.CATALOG);
-                bot.execute(message);
-                dataBase.initCursor();
-                dataBase.viewData(id, null, update.getMessage().getMessageId(), "/next:");
-                break;
-            case "/contact":
-                System.out.println("contact");
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText(MessageUser.CONTACT);
-                message.setReplyMarkup(Buttons.backButton());
-                bot.execute(message);
-                break;
-            default:
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText("Не вірна команда");
-                bot.execute(message);
-                break;
+        if (update.getMessage().getText().equals("/start")) {
+            sendMessages.sendStartMessage(update);
+        } else {
+            System.out.println("default");
+            //sendMessages.sendError(update);
         }
     }
     @SneakyThrows
     public void handleCallback(Update update, CallbackQuery callbackQuery) {
         System.out.println("callback");
-        dataBase.initCursor();
-
-        SendMessage message = new SendMessage();
 
         Message messages = (Message) callbackQuery.getMessage();
         String[] data = callbackQuery.getData().split(":");
@@ -73,54 +28,36 @@ public class UserHandle {
         switch (command) {
             case "/catalog":
                 System.out.println("catalog callback");
-                String idCallBack = update.getCallbackQuery().getMessage().getChatId().toString();
-                Integer messageId = messages.getMessageId();
-                message.setChatId(idCallBack);
-                message.setText(MessageUser.CATALOG);
-                bot.execute(message);
-               dataBase.viewData(idCallBack, update.getCallbackQuery().getId(), messageId, update.getCallbackQuery().getData());
+                sendMessages.sendCatalog(update, callbackQuery);
                 break;
 
             case "/contact":
-                System.out.println("contact");
-                message.setChatId(messages.getChatId().toString());
-                message.setText(MessageUser.CONTACT);
-                message.setReplyMarkup(Buttons.backButton());
-                bot.execute(message);
+                System.out.println("contact callback");
+                sendMessages.sendContact(update, callbackQuery);
                 break;
 
             case "/help":
                 System.out.println("help");
-                message.setChatId(messages.getChatId().toString());
-                message.setText(MessageUser.HELP);
-                message.setReplyMarkup(Buttons.backButton());
-                bot.execute(message);
+                sendMessages.sendHelp(update, callbackQuery);
                 break;
 
             case "/back":
                 System.out.println("back");
-                DataBase.editMode = 0;
-                message.setChatId(messages.getChatId().toString());
-                message.setText(MessageUser.STARTMESSAGEUSER);
-                message.setReplyMarkup(Buttons.mainButtons());
-                bot.execute(message);
+                sendMessages.sendBack(update, callbackQuery);
                 break;
 
             case "/next":
                 System.out.println("nextPage");
-                dataBase.viewData(messages.getChatId().toString(), update.getCallbackQuery().getId(), messages.getMessageId(), update.getCallbackQuery().getData());
+                sendMessages.sendNext(update, callbackQuery);
                 break;
 
             case "/backPage":
                 System.out.println("backPage");
-                dataBase.viewData(messages.getChatId().toString(), update.getCallbackQuery().getId(), messages.getMessageId(), update.getCallbackQuery().getData());
+                sendMessages.sendBackPage(update, callbackQuery);
                 break;
 
             default:
                 System.out.println("default");
-                message.setChatId(messages.getChatId().toString());
-                message.setText("Не вірна команда");
-                bot.execute(message);
                 break;
         }
     }
